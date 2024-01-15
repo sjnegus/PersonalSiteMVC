c470fd8ed4f3c2a6d4557906a8665d89fb71f709
Sam Negus <sam.negus@outlook.com>
2024-01-15 14:21:54 -0600

    Add project files.

---
+++ PersonalSite/Controllers/PorfolioController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalSite.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DungeonDweller () 
+        {
+            return View();
+        }
+
+        public IActionResult IntroToMVC ()
+        {
+            return View();
+        }
+    }
+}
---
+++ PersonalSite/Models/ContactViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations; // Grants easy access to annotations for validation
+
+namespace PersonalSite.Models
+{
+    public class ContactViewModel
+    {
+
+
+        [Required(ErrorMessage = "*Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*Subject is required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*Message is required")]
+        [DataType(DataType.MultilineText)] 
+        public string Message { get; set; }
+    }
+}
